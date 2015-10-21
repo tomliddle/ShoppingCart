fd6151dc16581321221c8844eb6e4930ad9ee703
Tom Liddle <tom.liddle@gmail.com>
2015-10-21 11:42:45 +0100

    Step 2 - added new methods and tests

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -2,14 +2,20 @@
 
 abstract class StoreItem {
 	val cost: BigDecimal
+	val dealGet: Int
+	val dealPayFor: Int
 }
 
 case class Apple() extends StoreItem {
 	override val cost: BigDecimal = 0.6
+	override val dealGet = 2
+	override val dealPayFor = 1
 }
 
 case class Orange() extends StoreItem {
 	override val cost: BigDecimal = 0.25
+	override val dealGet = 3
+	override val dealPayFor = 2
 }
 
 object ShoppingCart {
@@ -30,4 +36,20 @@ class ShoppingCart {
 	def calculateCost(itemList: List[StoreItem]): BigDecimal = {
 		itemList.map(_.cost).sum
 	}
+
+	def calculateDiscountCost(itemList: List[StoreItem]): BigDecimal = {
+		def costPerStoreItem(items: List[StoreItem]): BigDecimal = {
+			if (items.size == 0) 0
+			else {
+				//Remainder outside of deal
+				val remainder = items.size % items(0).dealGet
+				// Number of items needed to be bought for the max deals (excluding the remainder)
+				val noNeededToBuy = (items.size - remainder) * items(0).dealPayFor / items(0).dealGet
+				items(0).cost * (noNeededToBuy + remainder)
+			}
+		}
+
+		itemList.groupBy(identity).map{x => costPerStoreItem(x._2)}.sum
+	}
+
 }
--- src/test/scala/ShoppingCartTest.scala
+++ src/test/scala/ShoppingCartTest.scala
@@ -20,9 +20,47 @@ class ShoppingCartTest extends WordSpec with Matchers with BeforeAndAfterEach {
 				shoppingCart.calculateCost(list) should equal(BigDecimal(3.4))
 			}
 
+			"calculate 1 orange" in {
+				val list = List(Orange())
+				shoppingCart.calculateCost(list) should equal(BigDecimal(0.25))
+			}
+
+			"calculate 1 apple" in {
+				val list = List(Apple())
+				shoppingCart.calculateCost(list) should equal(BigDecimal(0.60))
+			}
+
 			"calculate an empty list" in {
 				val list = List()
 				shoppingCart.calculateCost(list) should equal(BigDecimal(0))
+			}
+		}
+
+		"calculating discount cost" should {
+
+			"calculate 3 apples 1 orange" in {
+				val list = List(Apple(), Apple(), Orange(), Apple())
+				shoppingCart.calculateDiscountCost(list) should equal(BigDecimal(1.45))
+			}
+
+			"calculate 4 apples 4 oranges" in {
+				val list = List(Apple(), Apple(), Orange(), Apple(), Apple(), Orange(), Orange(), Orange())
+				shoppingCart.calculateDiscountCost(list) should equal(BigDecimal(1.95))
+			}
+
+			"calculate 1 orange" in {
+				val list = List(Orange())
+				shoppingCart.calculateDiscountCost(list) should equal(BigDecimal(0.25))
+			}
+
+			"calculate 1 apple" in {
+				val list = List(Apple())
+				shoppingCart.calculateDiscountCost(list) should equal(BigDecimal(0.60))
+			}
+
+			"calculate an empty list" in {
+				val list = List()
+				shoppingCart.calculateDiscountCost(list) should equal(BigDecimal(0))
 			}
 		}
 	}
